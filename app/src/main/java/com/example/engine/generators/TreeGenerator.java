e725ada6e1cea0625247d7de33c694039158e008
josyvine <64621103+josyvine@users.noreply.github.com>
2026-08-11 10:37:17 +0530

    Update TreeGenerator.java

--- app/src/main/java/com/example/engine/generators/TreeGenerator.java
+++ app/src/main/java/com/example/engine/generators/TreeGenerator.java
@@ -16,27 +16,23 @@ public class TreeGenerator {
         Material barkMat = matMgr.getMaterial("mat_tree_bark");
         Material leafMat = matMgr.getMaterial("mat_foliage");
 
-        // 1. Root Tree Container Node
-        SceneObject treeRoot = new SceneObject(rootId, rootName, "TREE", null, null);
+        // 1. Root Tree Trunk (Vertical cylinder - acts as primary transform anchor)
+        SceneObject treeRoot = new SceneObject(rootId, rootName, "TREE",
+                PrimitiveGenerator.createCylinder(0.12f, 2.0f, 10), barkMat);
+        treeRoot.getTransform().setPosition(0f, 1.0f, 0f);
 
-        // 2. Main Trunk (Vertical bark cylinder)
-        SceneObject trunk = new SceneObject(rootId + "_trunk", "Tree Trunk", "STRUCTURE",
-                PrimitiveGenerator.createCylinder(0.12f, 2.0f, 8), barkMat);
-        trunk.getTransform().setPosition(0f, 1.0f, 0f);
-        treeRoot.addChild(trunk);
-
-        // 3. Main Foliage Canopy (Large green sphere on top of trunk)
+        // 2. Main Foliage Canopy (Large green sphere on top of the trunk)
         SceneObject mainCanopy = new SceneObject(rootId + "_canopy_main", "Foliage Main Crown", "STRUCTURE",
                 PrimitiveGenerator.createSphere(0.8f, 12, 12), leafMat);
-        mainCanopy.getTransform().setPosition(0f, 2.2f, 0f);
+        mainCanopy.getTransform().setPosition(0f, 1.2f, 0f);
         treeRoot.addChild(mainCanopy);
 
-        // 4. Procedural Supporting Branches (Angled cylinder struts extending outwards)
+        // 3. Procedural Supporting Branches (Angled cylinder struts extending outwards)
         float[][] branchData = new float[][] {
-                { -0.3f, 1.6f,  0.0f,  0f, 0f,  45f }, // [x, y, z, rx, ry, rz]
-                {  0.3f, 1.6f,  0.0f,  0f, 0f, -45f },
-                {  0.0f, 1.6f, -0.3f, -45f, 0f,  0f },
-                {  0.0f, 1.6f,  0.3f,  45f, 0f,  0f }
+                { -0.3f, 0.6f,  0.0f,  0f, 0f,  45.0f }, // [x, y, z, rx, ry, rz]
+                {  0.3f, 0.6f,  0.0f,  0f, 0f, -45.0f },
+                {  0.0f, 0.6f, -0.3f, -45.0f, 0f,  0f },
+                {  0.0f, 0.6f,  0.3f,  45.0f, 0f,  0f }
         };
 
         for (int i = 0; i < 4; i++) {
@@ -46,11 +42,11 @@ public class TreeGenerator {
             branch.getTransform().setRotation(branchData[i][3], branchData[i][4], branchData[i][5]);
             treeRoot.addChild(branch);
 
-            // 5. Secondary Foliage Spheres (Small green crowns at branch ends)
+            // 4. Secondary Foliage Spheres (Small green crowns at branch ends)
             SceneObject branchFoliage = new SceneObject(rootId + "_foliage_" + i, "Branch Foliage " + (i + 1), "STRUCTURE",
                     PrimitiveGenerator.createSphere(0.45f, 10, 10), leafMat);
             
-            // Position foliage at the tip of the angled branches
+            // Position foliage at the exact tip of the angled branches
             float fx = branchData[i][0] * 1.8f;
             float fy = branchData[i][1] + 0.2f;
             float fz = branchData[i][2] * 1.8f;
